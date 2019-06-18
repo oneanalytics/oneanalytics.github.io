4701ddc1dedd722ca2ca219c2c4f56fee7949731
charlest <charlestendeanz@gmail.com>
2019-06-18 18:54:35 +0700

    push all

---
+++ customScript.js
@@ -0,0 +1,20 @@
+var mainMenu = 
+              "<div class='topnav navbar' id='myTopnav'>"+
+                "<div><a href='#home'><img src='http://static1.squarespace.com/static/55669207e4b0783941de676d/t/574d3f9722482ec4b43c6499/1464680343835/IlmuOneData+-+Business+Card.png' style='width:200px;'></a>"+
+                  "</div>"+
+                    "<a href='#about'>"+
+                      "<i class='fas fa-calendar-alt' style='font-size: 24px;margin-right:5px;'></i>"+
+                      "<p>Event</p>"+
+                    "</a>"+
+                    "<a href='#about'>"+
+                    "<i class='fa fa-users' style='font-size: 24px;margin-right:5px;'></i>"+
+                      "<p>Solution</p>"+
+                    "</a>"+
+                    "<a href='#contact'>"+
+                    "<i class='fas fa-users-cog' style='font-size: 24px;margin-right:5px;'></i>"+
+                    "<p>Service</p>"+
+                  "</a>"+
+                  "<a href='javascript:void(0);' class='icon' onclick='myFunction()'>"+
+                      "<i class='fa fa-bars'></i>"+
+                  "</a>"+
+               "</div>"
