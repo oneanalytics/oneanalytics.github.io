e475cda2ec80a605f6bc6121e1b77625db0a4be2
charlest7 <charlestendeanz@gmail.com>
2020-08-17 14:42:04 +0700

    Update cookies.js

--- assets/js/cookies.js
+++ assets/js/cookies.js
@@ -13,3 +13,16 @@ function getCookie(cname) {
   }
   return "";
 }
+
+function deleteCookies() { 
+                var allCookies = document.cookie.split(';'); 
+                
+                // The "expire" attribute of every cookie is  
+                // Set to "Thu, 01 Jan 1970 00:00:00 GMT" 
+                for (var i = 0; i < allCookies.length; i++) 
+                    document.cookie = allCookies[i] + "=;expires=" 
+                    + new Date(0).toUTCString(); 
+  
+                displayCookies.innerHTML = document.cookie; 
+  
+            } 
