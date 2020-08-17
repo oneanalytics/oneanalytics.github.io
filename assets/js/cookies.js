7b5a0da62fe47f9bb7a56ba3f23d730d0a5211e1
charlest7 <charlestendeanz@gmail.com>
2020-08-17 15:48:58 +0700

    Update cookies.js

--- assets/js/cookies.js
+++ assets/js/cookies.js
@@ -21,6 +21,6 @@ function deleteCookies() {
                 // Set to "Thu, 01 Jan 1970 00:00:00 GMT" 
                 for (var i = 0; i < allCookies.length; i++) 
                     document.cookie = allCookies[i] + "=;expires="+new Date(0).toUTCString(); 
-    consoole.log("document "+document.cookie);
+    console.log("document "+document.cookie);
   
 }
